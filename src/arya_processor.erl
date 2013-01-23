37de184971628bfc2b03cf49bb24343719e655b4
Jtalk <me@jtalk.me>
2013-01-23 21:55:22 +0600

    arya_processor: fixed indentations, useless comments deleted, copyright notice updated.

--- src/arya_processor.erl
+++ src/arya_processor.erl
@@ -1,39 +1,38 @@
 %%
-%%	Copyright (C) 2012 Nazarenko Roman.
+%%  Copyright (C) 2012-2013 Roman Nazarenko.
 %%
-%%	This file is part of Arya.
+%%  This file is part of Arya.
 %%
-%%	Arya is free software: you can redistribute it and/or modify
-%%	it under the terms of the GNU General Public License as published by
-%%	the Free Software Foundation, either version 3 of the License, or
-%%	(at your option) any later version.
+%%  Arya is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
 %%
-%%	Arya is distributed in the hope that it will be useful,
-%%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%	GNU General Public License for more details.
+%%  Arya is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
 %%
-%%	You should have received a copy of the GNU General Public License
-%%	along with Arya.  If not, see <http://www.gnu.org/licenses/>.
+%%  You should have received a copy of the GNU General Public License
+%%  along with Arya.  If not, see <http://www.gnu.org/licenses/>.
 
-%%	Author: Nazarenko Roman <mailto: me@jtalk.me>
-%%	License: <http://www.gnu.org/licenses/gpl.html>
+%%  Author: Nazarenko Roman <mailto: me@jtalk.me>
+%%  License: <http://www.gnu.org/licenses/gpl.html>
+
+%% @author Roman Nazarenko <me@jtalk.me>
+%% @copyright 2012-2013 Roman Nazarenko
 
 -module(arya_processor).
 -behaviour(gen_server).
-
-%% Standard overproject types.
 -include("arya_types.hrl").
 
-%% Starting.
+%% Handling:
 -export([ start_link/0]).
-%% Start/stop callbacks.
--export([ init/1, terminate/2]).
-%% Processimg callback.
 -export([ process/2]).
-%% Events handling callbacks.
+
+%% Callbacks:
+-export([ init/1, terminate/2]).
 -export([ handle_call/3, handle_info/2, handle_cast/2]).
-%% Patching callback.
 -export([ code_change/3]).
 
 %% DNS header size.
@@ -58,77 +57,73 @@
 %%% process handling, so let it be.
 %%% -----------------------------------------------------
 
-%% Starting
 %%% @spec start_link() -> Result
-%%%		Result = {ok,Pid} | ignore | {error,Error}
-%%%		 Pid = pid()
-%%%		 Error = {already_started,Pid} | term()
-%%%	
+%%%    Result = {ok,Pid} | ignore | {error,Error}
+%%%     Pid = pid()
+%%%     Error = {already_started,Pid} | term()
+%%%  
 %%% @doc Creates new Arya processor.
 %%%
 start_link() ->
-	gen_server:start_link( arya_processor, [], []).
+  gen_server:start_link(arya_processor, [], []).
 
-%%% @see Module:init/1 in gen_fsm(3).
-init( _) ->
-	random:seed( now()),
-	report( 1, "Processor starting"),
-	{ ok, null}.
-	
-%%% @spec process( Message) -> Result
-%%%		Message = record(recv)
-%%%		Result = { stop, Reason, null}
-%%%		 Reason = normal | { error, Error}
-%%%		  Error = term()
-%%%	
+%%% @spec process(Message) -> Result
+%%%    Message = record(recv)
+%%%    Result = {stop, Reason, null}
+%%%     Reason = normal | {error, Error}
+%%%      Error = term()
+%%%  
 %%% @doc Main parsing routine starter.
 %%%
-process( Pid, Message) when is_record( Message, recv) ->
-	report( 1, "Processing message", Pid),
-	report( 3, "Message", Message),
-	gen_server:cast( Pid, Message).
-	
-	
-%%% @doc Main parsing routine of the processor.
-process( Message)  ->
-	try
-		{ ok, Parsed} = arya_parse:parse( Message), % we got the url array too
-		arya_decision:postprocess( Parsed), % making decision about this packet.	
-		{ stop, normal, null}
-	catch
-		error:Error ->
-			ID = binary:part( Message#recv.data, 0, 2),
-			report( 0, "Error in Processor", Error),
-			arya_common:send_back( Message#recv.from, { id, ID}),
-			{ stop, {error, Error}, null}
-	end.
-	
-%% Callback		
-%%% @see Module:handle_cast/2 in gen_server(3).
-handle_cast( Message, _) when is_record( Message, recv) ->
-	process( Message), % start handling cycle
-	{ stop, normal, null};
-handle_cast( Data, State) ->
-	report( 0, "Wrong cast in Processor", Data),
-	{ noreply, State}.
+process(Pid, Message) when is_record(Message, recv) ->
+  report(1, "Processing message", Pid),
+  report(3, "Message", Message),
+  gen_server:cast(Pid, Message).
+  
+%% Routines:
+  
+%% Main parsing routine of the processor.
+process(Message)  ->
+  try
+    {ok, Parsed} = arya_parse:parse(Message), % we got the url array too
+    arya_decision:postprocess(Parsed), % making decision about this packet.  
+    {stop, normal, null}
+  catch
+    error:Error ->
+      ID = binary:part(Message#recv.data, 0, 2), % First two bytes are ID.
+      report(0, "Error in Processor", Error),
+      arya_common:send_back(Message#recv.from, {id, ID}),
+      {stop, {error, Error}, null}
+  end.
+  
+%% Callbacks:
 
-%%% @see Module:handle_sync_event/4 in gen_fsm(3).
-handle_call( Data, _, State) ->
-	report( 0, "Wrong sync event in Processor",Data),
-	{ reply, ok, State}.
+init(_) ->
+  random:seed(now()),
+  report(1, "Processor starting"),
+  {ok, null}.
+  
+handle_cast(Message, _) when is_record(Message, recv) ->
+  process(Message),
+  {stop, normal, null};
+  
+handle_cast(Data, State) ->
+  report(0, "Wrong cast in Processor", Data),
+  {noreply, State}.
 
-%%% @see Module:handle_info/3 in gen_fsm(3).
-handle_info( Data, State) ->
-	report( 0, "Wrong info in Processor",Data),
-	{ noreply, State}.
-	
-%%% @see Module:terminate/3 in gen_fsm(3).
-terminate( Reason, _) ->
-	report( 1, "Terminating Processor"), 
-	report( 2, "Reason", Reason),
-	ok.
-	
-%%% @see Module:code_change/4 in gen_fsm(3).
-code_change( _, StateData, _) ->
-	report( 1, "Code changing in Processor"),
-	{ ok, StateData}.
+handle_call(Data, _, State) ->
+  report(0, "Wrong sync event in Processor",Data),
+  {reply, ok, State}.
+
+handle_info(Data, State) ->
+  report(0, "Wrong info in Processor",Data),
+  {noreply, State}.
+  
+terminate(Reason, _) ->
+  report(1, "Terminating Processor"), 
+  report(2, "Reason", Reason),
+  ok.
+
+code_change(_, StateData, _) ->
+  report(1, "Code changing in Processor"),
+  {ok, StateData}.
