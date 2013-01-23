cf832c624349fd24cacf3ff0b69a7a81f323b694
Jtalk <me@jtalk.me>
2013-01-23 22:04:07 +0600

    arya_proc_sup: copyright notice fixed, exports rearranged, child/0 description fixed.

--- src/arya_proc_sup.erl
+++ src/arya_proc_sup.erl
@@ -1,23 +1,26 @@
 %%
-%%	Copyright (C) 2012 Nazarenko Roman.
+%%  Copyright (C) 2012-2013 Roman Nazarenko.
 %%
-%%	This file is part of Arya.
+%%  This file is part of Arya.
 %%
-%%	Arya is free software: you can redistribute it and/or modify
-%%	it under the terms of the GNU General Public License as published by
-%%	the Free Software Foundation, either version 3 of the License, or
-%%	(at your option) any later version.
+%%  Arya is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
 %%
-%%	Arya is distributed in the hope that it will be useful,
-%%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%	GNU General Public License for more details.
+%%  Arya is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
 %%
-%%	You should have received a copy of the GNU General Public License
-%%	along with Arya.  If not, see <http://www.gnu.org/licenses/>.
+%%  You should have received a copy of the GNU General Public License
+%%  along with Arya.  If not, see <http://www.gnu.org/licenses/>.
 
-%%	Author: Nazarenko Roman <mailto: me@jtalk.me>
-%%	License: <http://www.gnu.org/licenses/gpl.html>
+%%  Author: Nazarenko Roman <mailto: me@jtalk.me>
+%%  License: <http://www.gnu.org/licenses/gpl.html>
+
+%% @author Roman Nazarenko <me@jtalk.me>
+%% @copyright 2012-2013 Roman Nazarenko
 
 -module(arya_proc_sup).
 -behaviour(supervisor).
@@ -25,15 +28,12 @@
 %% Debug
 -import(jdb, [report/3, report/2, appenv/3, getenv/1]).
 
-%% Starting
+%% Making new processor and supervisor starting.
 -export([ start_link/1]).
-
-%% Starting callback
--export([ init/1]).
-
-%% Creation of the new child
 -export([ child/0]).
 
+%% Callbacks:
+-export([ init/1]).
 
 %%% -----------------------------------------------------
 %%% This is the processors' supervisor of Arya server. 
@@ -48,49 +48,46 @@
 %%% and terminating as well as a worker adding method.
 %%% -----------------------------------------------------
 
-%% Starting
-
-%%% @spec start_link( Timing) -> Result
-%%%		Timing = { MaxR, MaxT, Await} 
-%%%		 MaxR = integer() > 0
-%%%		 MaxT = integer() > 0
-%%%		 Await = integer() >= 0 | infinity
-%%%		Result = startlink_ret()
+%%% @spec child() -> {ok, child()} | {error, term()}
+%%%    
+%%%  @doc Starts new Arya processor and returns its PID.
 %%%
-%%%	For details about Timing and Result see supervisor(3).
+child() ->
+  report( 1, "Child creating in Process supervisor"),
+  supervisor:start_child( ?MODULE, []).
+  
+%%% @spec start_link( Timing) -> Result
+%%%    Timing = { MaxR, MaxT, Await} 
+%%%     MaxR = integer() > 0
+%%%     MaxT = integer() > 0
+%%%     Await = integer() >= 0 | infinity
+%%%    Result = startlink_ret()
+%%%
+%%%  For details about Timing and Result see supervisor(3).
 %%%
 %%% @doc Initializes Arya processors and handles them.
 %%%
 start_link( { MaxR, MaxT, Await} ) ->
-	report( 1, "Starting processors' supervisor"),
-	supervisor:start_link(
-		{ local, arya_proc_sup },
-		arya_proc_sup,
-		{ MaxR, MaxT, Await}
-	).
+  report( 1, "Starting processors' supervisor"),
+  supervisor:start_link(
+    { local, arya_proc_sup },
+    arya_proc_sup,
+    { MaxR, MaxT, Await} %% @see supervisor manual.
+  ).
 
-%%% @see Module:init/1 in supervisor(3).
+%% Callbacks:
+
 init( { MaxR, MaxT, Await} ) ->
-	report( 1, "Process supervisor starting"),
-	{ ok, 
-		{
-			{ simple_one_for_one , MaxR, MaxT }, 
-			[
-				{ 	
-					arya_processor, { arya_processor, start_link, []},
-					temporary, Await, worker, [arya_processor] % See supervisor behaviour articles
-				}
-			]
-		}
-	}.
-			
-%% Routines			
-%%% @spec process( Message) -> ok | {error, Reason}
-%%%		Message = record( recv)
-%%%		
-%%%	@doc Starts new Arya processor to process a Message specified.
-%%%
-child() ->
-	report( 1, "Child creating in Process supervisor"),
-	supervisor:start_child( ?MODULE, []).
-	
+  report( 1, "Process supervisor starting"),
+  { ok, 
+    {
+      { simple_one_for_one , MaxR, MaxT }, 
+      [
+        {   
+          arya_processor, { arya_processor, start_link, []},
+          temporary, Await, worker, [arya_processor] % See supervisor behaviour articles
+        }
+      ]
+    }
+  }.
+  
