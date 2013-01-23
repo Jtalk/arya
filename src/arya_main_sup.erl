161f3bba0038154877a516e29d2dd41144e4ab53
Jtalk <me@jtalk.me>
2013-01-23 22:35:32 +0600

    arya_main_sup: fixed copyright notice, fixed indentation.

--- src/arya_main_sup.erl
+++ src/arya_main_sup.erl
@@ -1,28 +1,31 @@
 %%
-%%	Copyright (C) 2012 Nazarenko Roman.
+%%  Copyright (C) 2012-2013 Roman Nazarenko.
 %%
-%%	This file is part of Arya.
+%%  This file is part of Arya.
 %%
-%%	Arya is free software: you can redistribute it and/or modify
-%%	it under the terms of the GNU General Public License as published by
-%%	the Free Software Foundation, either version 3 of the License, or
-%%	(at your option) any later version.
+%%  Arya is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
 %%
-%%	Arya is distributed in the hope that it will be useful,
-%%	but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%	GNU General Public License for more details.
+%%  Arya is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
 %%
-%%	You should have received a copy of the GNU General Public License
-%%	along with Arya.  If not, see <http://www.gnu.org/licenses/>.
+%%  You should have received a copy of the GNU General Public License
+%%  along with Arya.  If not, see <http://www.gnu.org/licenses/>.
 
-%%	Author: Nazarenko Roman <mailto: me@jtalk.me>
-%%	License: <http://www.gnu.org/licenses/gpl.html>
+%%  Author: Nazarenko Roman <mailto: me@jtalk.me>
+%%  License: <http://www.gnu.org/licenses/gpl.html>
+
+%% @author Roman Nazarenko <me@jtalk.me>
+%% @copyright 2012-2013 Roman Nazarenko
 
 -module(arya_main_sup).
 -behaviour(supervisor).
 
-%% Debug
+%% Debug:
 -import(jdb, [report/3, report/2, appenv/3, ret/1]).
 
 %% Callback and start function 
@@ -31,10 +34,10 @@
 %%% -----------------------------------------------------
 %%% This is the main supervisor of Arya server. It looks 
 %%% after all the major parts of the server:
-%%% 	arya_server,
-%%%		arya_token_storage,
-%%%		arya_proc_sup, 
-%%%		arya_dl_sup.
+%%%   arya_server,
+%%%    arya_token_storage,
+%%%    arya_proc_sup, 
+%%%    arya_dl_sup.
 %%%
 %%% For further information about this modules see their 
 %%% documentation.
@@ -44,31 +47,31 @@
 %%% -----------------------------------------------------
 
 %%% @spec start_link( Timing) -> Result
-%%%		Timing = { MaxR, MaxT } 
-%%%		Result = startlink_ret()
+%%%    Timing = { MaxR, MaxT } 
+%%%    Result = startlink_ret()
 %%%
-%%%	For details about Timing and Result see supervisor(3).
+%%%  For details about Timing and Result see supervisor(3).
 %%%
 %%% @doc Initializes Arya major modules with the supervision timings provided
 %%%
 start_link( { MaxR, MaxT} ) ->
-	supervisor:start_link( 
-		{ local, ?MODULE }, 
-		?MODULE, 
-		{ MaxR, MaxT}
-	).
-	
+  supervisor:start_link( 
+    { local, ?MODULE }, 
+    ?MODULE, 
+    { MaxR, MaxT}
+  ).
+  
 %%% @see Module:init/1 in supervisor(3).
-init( { MaxR, MaxT} ) ->
-	report( 1, "Arya main supervisor initializing"),
-	{ ok, 
-		{ 
-			{ one_for_one, MaxR, MaxT },
-			[  %% Internal name			 Start module name		Start func	Start arguments			Restart type	Exit timeout	Process type	Modules needed
-				{ arya_server, 			{ arya_server,			start_link, [null]}, 				transient, 		1000, 			worker, 		[arya_server] },
-				{ arya_token_storage, 	{ arya_token_storage,	start_link,	[null]}, 				transient, 		100, 			worker, 		[arya_token_storage]},
-				{ arya_proc_sup, 		{ arya_proc_sup, 		start_link, [{ MaxR, MaxT, 100}]}, 	transient, 		infinity,		supervisor, 	[arya_proc_sup]},
-				{ arya_dl_sup, 			{ arya_dl_sup, 			start_link, [{ MaxR, MaxT, 100}]}, 	transient, 		infinity,		supervisor, 	[arya_dl_sup]}				
-			]
-		}
-	}.
+init({ MaxR, MaxT} ) ->
+  report(1, "Arya main supervisor initializing"),
+  {ok, 
+    { 
+      {one_for_one, MaxR, MaxT },
+      [%<Internal name> <Module name> <Start func> <Arguments> <Restart type> <Exit timeout> <Process type> <Depends>
+        { arya_server, {arya_server, start_link, [null]}, transient, 1000, worker, [arya_server]},
+        { arya_token_storage, { arya_token_storage, start_link, [null]}, transient, 100, worker, [arya_token_storage]},
+        { arya_proc_sup, {arya_proc_sup, start_link, [{ MaxR, MaxT, 100}]}, transient, infinity, supervisor, [arya_proc_sup]},
+        { arya_dl_sup, { arya_dl_sup, start_link, [{ MaxR, MaxT, 100}]}, transient, infinity, supervisor, [arya_dl_sup]}        
+      ]
+    }
+  }.
